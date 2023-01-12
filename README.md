a73c78dd291b04ee0d2f102a27a8978a5ddf7d0e
Pat Tep <patonthestack@gmail.com>
2023-01-11 22:38:24 -0500

    mock json server config

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Feedback App
 
-# Built with React JS and deployed on Netlify at: https://frabjous-horse-0f1e8f.netlify.app
+## Built with React JS and a mock JSON server
 
-## Basic feedback interface with full CRUD operations for leaving a review for a product or service
+## App is deployed on Netlify at: https://frabjous-horse-0f1e8f.netlify.app
+
+### Basic feedback tool with full CRUD operations for leaving a review for a product or service
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,43 +1,59 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { createContext, useEffect, useState } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is feedback item 1',
-      rating: 5,
-    },
-    {
-      id: 2,
-      text: 'This item is feedback item 2',
-      rating: 4,
-    },
-    {
-      id: 3,
-      text: 'This item is feedback item 3',
-      rating: 2,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
 
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch('/feedback?_sort=id&_order=desc');
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
   // Add new feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
     // take objects that are already in 'feedback' array as well as 'newFeedback'
-    setFeedback([newFeedback, ...feedback]);
+    setFeedback([data, ...feedback]);
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)),
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item)),
     );
   };
 
@@ -47,8 +63,9 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Delete existing feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -58,6 +75,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
