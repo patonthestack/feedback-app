ae1de66b8579be39cd9d9540b25091148c7f5eaf
Pat Tep <patonthestack@gmail.com>
2023-01-19 17:51:44 -0500

    revert back to no json server for prod

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,67 +1,108 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const [feedback, setFeedback] = useState([]);
+
+  // with json server
+  // const [feedback, setFeedback] = useState([]);
+
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This item is feedback item 1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This item is feedback item 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This item is feedback item 3',
+      rating: 8,
+    },
+  ]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
 
+  // only to be used with json server
+  // useEffect(() => {
+  //   fetchFeedback();
+  // }, []);
+
+  // // Fetch feedback
+  // const fetchFeedback = async () => {
+  //   const response = await fetch(
+  //     `${process.env.REACT_APP_BASE_URL}:5000/feedback?_sort=id&_order=desc`,
+  //   );
+  //   const data = await response.json();
+
+  //   setFeedback(data);
+  //   setIsLoading(false);
+  // };
+
   useEffect(() => {
-    fetchFeedback();
-  }, []);
-
-  // Fetch feedback
-  const fetchFeedback = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_BASE_URL}:5000/feedback?_sort=id&_order=desc`,
-    );
-    const data = await response.json();
-
-    setFeedback(data);
     setIsLoading(false);
-  };
+  }, [isLoading]);
+
+  // Add new feedback, only to be used with json server
+  // const addFeedback = async (newFeedback) => {
+  //   const response = await fetch(
+  //     `${process.env.REACT_APP_BASE_URL}:5000/feedback`,
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(newFeedback),
+  //     },
+  //   );
+
+  //   const data = await response.json();
+
+  //   // take objects that are already in 'feedback' array as well as 'newFeedback'
+  //   setFeedback([data, ...feedback]);
+  // };
 
   // Add new feedback
-  const addFeedback = async (newFeedback) => {
-    const response = await fetch(
-      `${process.env.REACT_APP_BASE_URL}:5000/feedback`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newFeedback),
-      },
-    );
-
-    const data = await response.json();
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
 
     // take objects that are already in 'feedback' array as well as 'newFeedback'
-    setFeedback([data, ...feedback]);
+    setFeedback([newFeedback, ...feedback]);
   };
 
+  // Update feedback item, json server version
+  // const updateFeedback = async (id, updItem) => {
+  //   const response = await fetch(
+  //     `${process.env.REACT_APP_BASE_URL}:5000/feedback/${id}`,
+  //     {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(updItem),
+  //     },
+  //   );
+
+  //   const data = response.json();
+
+  //   setFeedback(feedback.map((item) => (item.id === id ? data : item)));
+  //   setFeedbackEdit({ item: {}, edit: false });
+  // };
+
   // Update feedback item
-  const updateFeedback = async (id, updItem) => {
-    const response = await fetch(
-      `${process.env.REACT_APP_BASE_URL}:5000/feedback/${id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updItem),
-      },
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)),
     );
-
-    const data = response.json();
-
-    setFeedback(feedback.map((item) => (item.id === id ? data : item)));
-    setFeedbackEdit({ item: {}, edit: false });
   };
 
   // Set item to be updated
@@ -69,12 +110,18 @@ export const FeedbackProvider = ({ children }) => {
     setFeedbackEdit({ item, edit: true });
   };
 
-  // Delete existing feedback
-  const deleteFeedback = async (id) => {
+  // Delete existing feedback, json server version
+  // const deleteFeedback = async (id) => {
+  //   if (window.confirm('Are you sure you want to delete?')) {
+  //     await fetch(`${process.env.REACT_APP_BASE_URL}:5000/feedback/${id}`, {
+  //       method: 'DELETE',
+  //     });
+  //     setFeedback(feedback.filter((item) => item.id !== id));
+  //   }
+  // };
+
+  const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
-      await fetch(`${process.env.REACT_APP_BASE_URL}:5000/feedback/${id}`, {
-        method: 'DELETE',
-      });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
